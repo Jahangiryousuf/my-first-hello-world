4b82ba0648ffc680ee241c439fd2caf154978684
Jahangiryousuf <160110472+Jahangiryousuf@users.noreply.github.com>
2024-03-02 02:56:38 +0500

    2nd and 3rd class

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("hi i am jj");
+var box = 'apple';
+var container = "ball";
+var myVariable = 100;
+console.log(box);
+1;
---
+++ main.ts
@@ -0,0 +1,9 @@
+console.log("hi i am jj");
+let box = 'apple';
+let container = "ball";
+let myVariable = 100
+console.log(box);
+console.log(container);
+console.log(myVariable);
+
+
